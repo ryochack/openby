e01abcd57b8d95d3ccc486ecf011721b55215e02
ryochack <ryochack@gmail.com>
2017-06-18 16:12:01 +0900

    apply rustfmt

--- src/main.rs
+++ src/main.rs
@@ -86,19 +86,31 @@ fn test_decode_toml() {
     let config: Config = toml::from_str(toml_config_str).unwrap();
     assert_eq!(config.version, toml::Value::Float(0.0));
 
-    assert_eq!(config.tools[0].command, toml::Value::String("apvlv".to_string()));
-    assert_eq!(config.tools[0].extentions[0], toml::Value::String("pdf".to_string()));
+    assert_eq!(config.tools[0].command,
+               toml::Value::String("apvlv".to_string()));
+    assert_eq!(config.tools[0].extentions[0],
+               toml::Value::String("pdf".to_string()));
 
-    assert_eq!(config.tools[1].command, toml::Value::String("mirage".to_string()));
-    assert_eq!(config.tools[1].extentions[0], toml::Value::String("jpg".to_string()));
-    assert_eq!(config.tools[1].extentions[1], toml::Value::String("png".to_string()));
-    assert_eq!(config.tools[1].extentions[2], toml::Value::String("gif".to_string()));
+    assert_eq!(config.tools[1].command,
+               toml::Value::String("mirage".to_string()));
+    assert_eq!(config.tools[1].extentions[0],
+               toml::Value::String("jpg".to_string()));
+    assert_eq!(config.tools[1].extentions[1],
+               toml::Value::String("png".to_string()));
+    assert_eq!(config.tools[1].extentions[2],
+               toml::Value::String("gif".to_string()));
 
-    assert_eq!(config.tools[2].command, toml::Value::String("vlc".to_string()));
-    assert_eq!(config.tools[2].extentions[0], toml::Value::String("mp4".to_string()));
-    assert_eq!(config.tools[2].extentions[1], toml::Value::String("mov".to_string()));
-    assert_eq!(config.tools[2].extentions[2], toml::Value::String("avi".to_string()));
+    assert_eq!(config.tools[2].command,
+               toml::Value::String("vlc".to_string()));
+    assert_eq!(config.tools[2].extentions[0],
+               toml::Value::String("mp4".to_string()));
+    assert_eq!(config.tools[2].extentions[1],
+               toml::Value::String("mov".to_string()));
+    assert_eq!(config.tools[2].extentions[2],
+               toml::Value::String("avi".to_string()));
 
-    assert_eq!(config.tools[3].command, toml::Value::String("vim -R".to_string()));
-    assert_eq!(config.tools[3].extentions[0], toml::Value::String("conf".to_string()));
+    assert_eq!(config.tools[3].command,
+               toml::Value::String("vim -R".to_string()));
+    assert_eq!(config.tools[3].extentions[0],
+               toml::Value::String("conf".to_string()));
 }
